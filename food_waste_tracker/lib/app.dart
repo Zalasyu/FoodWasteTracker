09c4c668c958cb38951ffef190f7eec10edf615b
Zalasyu <alec_moldovan@yahoo.com>
2022-08-09 19:32:21 -0500

    Took out named routes.

--- food_waste_tracker/lib/app.dart
+++ food_waste_tracker/lib/app.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:food_waste_tracker/screens/list_screen.dart';
-import 'package:food_waste_tracker/screens/new_post_screen.dart';
-import 'package:food_waste_tracker/screens/detail_screen.dart';
 
 class App extends StatefulWidget {
   @override
@@ -9,24 +7,21 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  static const routes = <String, WidgetBuilder>{
-    '/list': (BuildContext context) => const ListScreen(),
-    '/new': (BuildContext context) => NewPostScreen(),
-    '/detail': (BuildContext context) => const DetailScreen(),
-  };
-
-
   @override
   void initState() {
     super.initState();
   }
 
+  // TODO: Add a drawer to the app.
+
+  // build the app's home screen
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ), routes: ,
+      ),
+      home: ListScreen(),
     );
   }
 }
